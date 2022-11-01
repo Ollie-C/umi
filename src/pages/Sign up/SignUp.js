b12aac8406411bd425d7bf47c789e9d31d3b4a28
Ollie Cross <iio.olivercross@gmail.com>
2022-11-01 18:52:18 +0000

    Basic styling and layout for form

--- src/pages/Sign up/SignUp.js
+++ src/pages/Sign up/SignUp.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./SignUp.scss";
 
 const SignUp = ({ isLoggedIn }) => {
@@ -6,7 +7,77 @@ const SignUp = ({ isLoggedIn }) => {
       {!isLoggedIn ? (
         <>
           <h1 className="signup__header">Sign Up</h1>
-          <form action="" className="signup__form"></form>
+          <form id="signupform" className="signup__form">
+            <div className="signup__fields">
+              <label htmlFor="email" className="signup__label">
+                EMAIL:
+              </label>
+              <input
+                type="text"
+                className="signup__input"
+                name="email"
+                placeholder="Enter your email"
+              />
+            </div>
+            <div className="signup__fields">
+              <label htmlFor="username" className="signup__label">
+                USERNAME:
+              </label>
+              <input
+                type="text"
+                className="signup__input"
+                name="username"
+                placeholder="Pick a username"
+              />
+            </div>
+            <div className="signup__fields">
+              <label htmlFor="password" className="signup__label">
+                PASSWORD:
+              </label>
+              <input
+                type="text"
+                className="signup__input"
+                name="password"
+                placeholder="Enter a password between 3 - 16 characters long"
+              />
+            </div>
+            <div className="signup__fields">
+              <label htmlFor="confirmpassword" className="signup__label">
+                CONFIRM PASSWORD:
+              </label>
+              <input
+                type="text"
+                className="signup__input"
+                name="confirmpassword"
+                placeholder="Confirm your password"
+              />
+            </div>
+            <div className="signup__radiofield">
+              <p className="signup__label">
+                What kind of account do you want to make?
+              </p>
+
+              <input type="radio" className="signup__radio" name="type" />
+              <label htmlFor="personal" className="signup__label">
+                PERSONAL
+              </label>
+
+              <input
+                type="radio"
+                className="signup__radio"
+                name="organisation"
+              />
+              <label htmlFor="organisation" className="signup__label">
+                ORGANISATION
+              </label>
+            </div>
+            <div className="signup__button-wrapper">
+              <button className="signup__button">SUBMIT</button>
+              <Link className="signup__cancel" to="/">
+                CANCEL
+              </Link>
+            </div>
+          </form>
         </>
       ) : (
         <p className="signup__error">You already have an account.</p>
