fe9aeda3b31628a6f6dd7639c5495829b1089d30
Ollie Cross <iio.olivercross@gmail.com>
2022-11-01 19:26:20 +0000

    Set up basic post route without any validation

--- src/pages/Sign up/SignUp.js
+++ src/pages/Sign up/SignUp.js
@@ -1,13 +1,44 @@
+import { addDoc, collection } from "firebase/firestore";
+import { db, auth } from "../../fb-config";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./SignUp.scss";
 
 const SignUp = ({ isLoggedIn }) => {
+  const [isOrganisation, setIsOrganisation] = useState(false);
+  const [newUser, setNewUser] = useState();
+
+  const usersRef = collection(db, "users");
+
+  const createUser = async () => {
+    const accountType = isOrganisation ? "organisation" : "personal";
+    await addDoc(usersRef, {
+      email: newUser.email,
+      username: newUser.username,
+      account_type: accountType,
+    });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewUser({ ...newUser, [name]: value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    createUser();
+  };
+
   return (
     <div className="signup">
       {!isLoggedIn ? (
         <>
           <h1 className="signup__header">Sign Up</h1>
-          <form id="signupform" className="signup__form">
+          <form
+            id="signupform"
+            className="signup__form"
+            onSubmit={submitHandler}
+          >
             <div className="signup__fields">
               <label htmlFor="email" className="signup__label">
                 EMAIL:
@@ -17,6 +48,7 @@ const SignUp = ({ isLoggedIn }) => {
                 className="signup__input"
                 name="email"
                 placeholder="Enter your email"
+                onChange={(e) => handleChange(e)}
               />
             </div>
             <div className="signup__fields">
@@ -28,6 +60,7 @@ const SignUp = ({ isLoggedIn }) => {
                 className="signup__input"
                 name="username"
                 placeholder="Pick a username"
+                onChange={(e) => handleChange(e)}
               />
             </div>
             <div className="signup__fields">
@@ -35,10 +68,11 @@ const SignUp = ({ isLoggedIn }) => {
                 PASSWORD:
               </label>
               <input
-                type="text"
+                type="password"
                 className="signup__input"
                 name="password"
                 placeholder="Enter a password between 3 - 16 characters long"
+                onChange={(e) => handleChange(e)}
               />
             </div>
             <div className="signup__fields">
@@ -46,33 +80,40 @@ const SignUp = ({ isLoggedIn }) => {
                 CONFIRM PASSWORD:
               </label>
               <input
-                type="text"
+                type="password"
                 className="signup__input"
                 name="confirmpassword"
                 placeholder="Confirm your password"
+                onChange={(e) => handleChange(e)}
               />
             </div>
             <div className="signup__radiofield">
               <p className="signup__label">
                 What kind of account do you want to make?
               </p>
-
-              <input type="radio" className="signup__radio" name="type" />
-              <label htmlFor="personal" className="signup__label">
-                PERSONAL
-              </label>
-
               <input
                 type="radio"
                 className="signup__radio"
+                name="type"
+                value="personal"
+              />
+              <label htmlFor="personal" className="signup__label">
+                PERSONAL
+              </label>
+              {/* <input
+                type="radio"
+                className="signup__radio"
                 name="organisation"
+                value="organisation"
               />
               <label htmlFor="organisation" className="signup__label">
                 ORGANISATION
-              </label>
+              </label> */}
             </div>
             <div className="signup__button-wrapper">
-              <button className="signup__button">SUBMIT</button>
+              <button className="signup__button" form="signupform">
+                SUBMIT
+              </button>
               <Link className="signup__cancel" to="/">
                 CANCEL
               </Link>
