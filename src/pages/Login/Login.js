a28f56e869794972c7ef43d3926b27d196c3bab3
Ollie Cross <iio.olivercross@gmail.com>
2022-11-20 15:39:23 +0000

    Login styling complete

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -29,7 +29,7 @@ const Login = () => {
         <h1 className="login__header">Welcome to</h1>
         <img className="login__logo" src={logo} alt="umi logo" />
         <button className="login__google" onClick={handleLogIn}>
-          Log in with Google
+          LOG IN WITH GOOGLE
         </button>
         <p className="login__noaccount">
           Don't have a Google account? Log in with email and password.
