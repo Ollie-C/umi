c328975924946f6bcc2cd6a1b116b0cf0c3b8773
Ollie Cross <iio.olivercross@gmail.com>
2023-03-19 16:35:07 +0000

    :lipstick: Massive styling overhaul

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,34 +5,34 @@ import coin from "../../assets/images/unnamed.png";
 
 const Card = ({ result }) => {
   return (
-    <>
-      <div className="card">
-        <div className="card__imageWrapper">
-          <img
-            src={result.image ? result.image : OriginCoffee}
-            alt={`${result.name} image`}
-            className="card__image"
-          />
-        </div>
-        <div className="card__top">
-          <h2 className="card__title">
-            {result.name ? result.name : "No name."}
-          </h2>
-          <div className="coin-wrapper">
-            <p className="card__points">20</p>
-            <img className="itemcard__coin" src={coin} alt="coin icon" />
-          </div>
-        </div>
+    <div className="card">
+      <div
+        className="card__image"
+        style={{
+          backgroundImage: `url(${result.image ? result.image : OriginCoffee})`,
+        }}
+      ></div>
+
+      <div className="card__contents">
+        <h2 className="card__title">
+          {result.name ? result.name : "No name."}
+        </h2>
         <div className="card__description">
-          <p>{result.category}</p>
+          <p>{result.category ? result.category : "Other"}</p>
           <p className="card__text">BYOB | ZERO-WASTE | ORGANIC</p>
-          <div className="card__stats">
-            <Icon icon="lucide:verified" color="black" height="20" />
-            <p className="card__text">VERIFIED</p>
+          <div className="card__bottom">
+            <div className="card__bottom-section">
+              <Icon icon="lucide:verified" color="black" height="20" />
+              <p className="card__text">VERIFIED</p>
+            </div>
+            <div className="card__bottom-section">
+              <p className="card__points">20</p>
+              <img className="card__coin" src={coin} alt="coin icon" />
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
